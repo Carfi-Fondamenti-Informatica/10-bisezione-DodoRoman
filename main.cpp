5ef09a44a1141f209b36e58d5736604a7af24c3f
DodoRoman <115349003+DodoRoman@users.noreply.github.com>
2022-12-21 13:07:15 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
+double funzione(double y){
+    float risultato=pow(y,2)*cos(y)+1;
+    return risultato ;
+}
+
 int main() {
-   return 0;
+    double a=0,b=0,err=0,x=0;
+    do {
+        cout << "inserire estremi" << endl;
+        cin >> a;
+        cin >> b;
+    }while ((funzione(a) * funzione(b)) >= 0);
+    if (funzione(a) < funzione(b)){
+        b=a+b;
+        a=b-a;
+        b=b-a;
+    }
+    do {
+        x=(a+b)/2;
+
+        if (funzione(x)==0){
+            break;
+        }
+
+        if ((funzione(x))<0){
+            b=x;
+        }else {
+            a=x;
+        }
+
+
+        err = abs(((b-a)/2));
+    }while(err >= 0.000001);
+
+    int c=x*1000;
+    double d=(float)c/1000;
+    cout << d << endl;
+    return 0;
 }
